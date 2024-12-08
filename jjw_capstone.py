98e7fa5d56a1465eeaf93e720bd6ee4c484f202b
jj4252 <jj4252@nyu.edu>
2024-12-08 16:00:20 -0500

    q7-q10

--- jjw_capstone.py
+++ jjw_capstone.py
@@ -251,3 +251,139 @@ def plot_sampling_distribution(bootstrap_results, observed_effect, confidence_in
 plot_sampling_distribution(mean_bootstrap, mean_effect, mean_ci, title="Mean Difference (Cohen's d)")
 
 
+## ------------------------------------------------------------------------------------------------------------------------------ 
+# Q7
+
+
+def linear_regression_analysis(data, target_column, predictor_columns, na_handling= None, na_field = None, test_size=0.2):
+    # Handle NA values based on the selected option
+    if na_handling == "Row Drop":
+        data_selected = data.dropna()  # Drop rows with NA values
+        y = data_selected[target_column]
+        X = data_selected[predictor_columns]
+    elif na_handling == "Column Drop":
+        data_selected = data.copy()
+        y = data_selected[target_column]
+        X = data_selected[predictor_columns].drop([na_field], axis=1)  
+    else:
+        X = data[predictor_columns]
+        y = data[target_column]
+   
+ 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    
+    y_pred_train = model.predict(X_train)
+    X_test = scaler.transform(X_test)
+    y_pred_test = model.predict(X_test)
+    
+    r2 = r2_score(y_train, y_pred_train)  
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred_test))
+    print(len(X))
+    for i in range(len(model.coef_)):
+        print(f"{predictor_columns[i]} : {model.coef_[i]}")
+    return r2, rmse, model.coef_
+
+r2, rmse, coef = linear_regression_analysis(num_with_gender, target_column = 'average_ratings', predictor_columns = [x for x in num_columns if x not in  ['average_ratings', 'is_male']]
+, na_handling= 'Row Drop', na_field = 'prop_take_again')
+
+print(r2, rmse)
+
+
+## ------------------------------------------------------------------------------------------------------------------------------ 
+# Q8
+
+dat = normalize_tags(num_with_gender[['is_female', 'num_ratings', 'average_ratings', 'prop_take_again'] + tag_columns], tag_columns).drop(['num_ratings'], axis = 1)
+
+r2, rmse,coef = linear_regression_analysis(dat, target_column = 'average_ratings', predictor_columns=['is_female'] + tag_columns, na_handling= 'Row Drop', na_field = None, test_size=0.2)
+print(r2, rmse)
+
+
+## ------------------------------------------------------------------------------------------------------------------------------ 
+# Q9 
+dat = normalize_tags(num_with_gender[['is_female', 'num_ratings', 'average_difficulty'] + tag_columns], tag_columns).drop(['num_ratings'], axis = 1)
+
+r2, rmse,coef = linear_regression_analysis(dat, target_column = 'average_difficulty', predictor_columns=['is_female'] + tag_columns, na_handling= 'Row Drop', na_field =  None, test_size=0.2)
+print(r2, rmse)
+
+
+
+
+## ------------------------------------------------------------------------------------------------------------------------------ 
+# Q10 
+
+def classifier(data, target_column, predictor_columns, test_size=0.2, mod = 'logistic', threshold = 0.3):
+    X = data[predictor_columns]
+    y = data[target_column]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    
+    if mod == 'logistic':
+        model = LogisticRegression()
+        model.fit(X_train, y_train)
+    elif mod == 'svm':
+        model = SVC(kernel='rbf', probability=True)  
+        model.fit(X_train, y_train)
+    elif mod == 'rf':
+        model = RandomForestClassifier(n_estimators=100, max_depth=None)
+        model.fit(X_train, y_train)
+    y_pred_train = model.predict(X_train)
+    X_test = scaler.transform(X_test)
+    y_pred_test = model.predict(X_test)
+    
+    y_pred_proba = model.predict_proba(X_test)[:, 1]
+    # y_pred = model.predict(X_test)
+    y_pred = (y_pred_proba >= threshold).astype(int)
+
+  
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+
+    print('accuracy:', accuracy)
+
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix:\n", conf_matrix)
+
+    auc = roc_auc_score(y_test, y_pred_proba)
+    print(f"AU(RO)C: {auc:.3f}")
+
+    f1 = f1_score(y_test, y_pred)
+    print(f"F-1 Score : {f1}")
+
+  
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, label=f'AU(RO)C = {auc:.3f}')
+    plt.plot([0, 1], [0, 1], 'k--')  
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend(loc="lower right")
+    plt.show()
+
+  
+    return {
+        "model": model,
+        "AU(RO)C": auc,
+        "confusion_matrix": conf_matrix,
+        # "roc_curve": (fpr, tpr, thresholds),
+        "f1 score" : f1,
+        'accuracy' : accuracy,
+        'precision': precision,
+        'recall' : recall 
+    }
+
+
+dat = num_with_gender[num_columns + tag_columns].drop('is_male', axis = 1).dropna()
+predictors = [x for x in dat.columns if x != 'is_received_pepper']
+metrics= classifier(dat, target_column= 'is_received_pepper', predictor_columns= predictors, mod = 'logistic', threshold=0.5)
+
